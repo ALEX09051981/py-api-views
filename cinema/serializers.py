a93d1c3a2310cea595761d2636661e17383a54cb
Alexey <alopar632@gmail.com>
2025-07-10 18:18:36 +0300

    Solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from cinema.models import Movie, Genre, CinemaHall, Actor
 
+
 class GenreSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=255)
@@ -27,7 +28,9 @@ class CinemaHallSerializer(serializers.Serializer):
     def update(self, instance, validated_data):
         instance.name = validated_data.get("name", instance.name)
         instance.rows = validated_data.get("rows", instance.rows)
-        instance.seats_in_row = validated_data.get("seats_in_row", instance.seats_in_row)
+        instance.seats_in_row = validated_data.get("seats_in_row",
+                                                   instance.seats_in_row
+                                                   )
         instance.save()
         return instance
 
@@ -41,8 +44,12 @@ class ActorSerializer(serializers.Serializer):
         return Actor.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.first_name = validated_data.get("first_name", instance.first_name)
-        instance.last_name = validated_data.get("last_name", instance.last_name)
+        instance.first_name = validated_data.get("first_name",
+                                                 instance.first_name
+                                                 )
+        instance.last_name = validated_data.get("last_name",
+                                                instance.last_name
+                                                )
         instance.save()
         return instance
 
@@ -58,7 +65,9 @@ class MovieSerializer(serializers.Serializer):
 
     def update(self, instance, validated_data):
         instance.title = validated_data.get("title", instance.title)
-        instance.description = validated_data.get("description", instance.description)
+        instance.description = validated_data.get("description",
+                                                  instance.description
+                                                  )
         instance.duration = validated_data.get("duration", instance.duration)
         instance.save()
         return instance
