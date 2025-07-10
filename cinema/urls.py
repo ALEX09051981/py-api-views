fdd12f4fff7d451f04b9be04d3d0c29a6ece35ec
Alexey <alopar632@gmail.com>
2025-07-10 18:12:57 +0300

    Solution

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,54 +1,22 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
-from cinema.views import (
-    movie_list,
-    movie_detail,
-    GenreListCreateAPIView,
-    GenreRetrieveUpdateDestroyAPIView,
-    ActorListCreateAPIView,
-    ActorRetrieveUpdateDestroyAPIView,
-    CinemaHallViewSet,
-    MovieViewSet,
-)
+from cinema.views import (MovieViewSet,
+                          CinemaHallViewSet,
+                          ActorList, ActorDetail,
+                          GenreList, GenreDetail
+                          )
 
 app_name = "cinema"
 
-urlpatterns = [
-    path(
-        "movies/",
-        movie_list,
-        name="movie-list"
-    ),
-    path(
-        "movies/<int:pk>/",
-        movie_detail,
-        name="movie-detail"
-    ),
-    path(
-        "genres/",
-        GenreListCreateAPIView.as_view(),
-        name="genre-list-create"
-    ),
-    path(
-        "genres/<int:pk>/",
-        GenreRetrieveUpdateDestroyAPIView.as_view(),
-        name="genre-detail"
-    ),
-    path(
-        "actors/",
-        ActorListCreateAPIView.as_view(),
-        name="actor-list-create"
-    ),
-    path(
-        "actors/<int:pk>/",
-        ActorRetrieveUpdateDestroyAPIView.as_view(),
-        name="actor-detail"
-    ),
-]
 
 router = DefaultRouter()
-router.register(r"cinema-halls", CinemaHallViewSet, basename="cinema-hall")
-router.register(r"movies", MovieViewSet, basename="movie")
+router.register(r"movies", MovieViewSet)
+router.register(r"cinema_halls", CinemaHallViewSet)
 
-urlpatterns += router.urls
+urlpatterns = [
+    path("", include(router.urls)),
+    path("actors/", ActorList.as_view(), name="actor-list"),
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
+    path("genres/", GenreList.as_view(), name="genre-list"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
+]
